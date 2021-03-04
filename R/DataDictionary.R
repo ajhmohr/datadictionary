3903086bc1d07f4f2daca2d52fbd1ccc5707352b
Alicia Hofelich Mohr <hofelich@umn.edu>
2021-03-04 08:53:21 -0600

    updated to work with stata files

--- R/DataDictionary.R
+++ R/DataDictionary.R
@@ -16,50 +16,74 @@
 
 DataDictionary <- function(x, variable_labels = variable_labels, value_labels = value_labels, include.summary=FALSE, file="DataDictionary.txt") {
   
-  #foreign-style variable labels
+  #foreign-style SPSS variable labels
   if (!is.null(attr(x, "variable.labels"))) {
     
     variable_labels <- attr(x, "variable.labels")
     
-  } else {
-    
-  #haven-style variable labels
-    if (!is.null(unlist(sapply(x, attr, "label")))) {
+  } 
+  
+  #foreign-style Stata variable labels
+  else if (!is.null(attr(x, "var.labels"))) {
+      
+      variable_labels <- attr(x, "var.labels")
+      
+    } 
+  
+  #haven-style SPSS or Stata variable labels
+  else if (!is.null(unlist(sapply(x, attr, "label")))) {
       
       variable_labels <- sapply(x, attr, "label")
       
     }
     
-  }
   
-  #foreign-style value labels
+  #foreign-style SPSS value labels
   if (!is.null(unlist(sapply(x, attr, "value.labels")))) {
     
     value_labels <- sapply(x, attr, "value.labels")
     
-  } else {
+  } 
+  
+  #foreign-style Stata value labels
+  else if (!is.null(attr(x, "label.table"))) {
     
-    #haven-style variable labels
-    if (!is.null(unlist(sapply(x, attr, "labels", exact=TRUE)))) {
+    value_labels <- as.list(rep("", ncol(x)))
+    names(value_labels) <- names(x)
+    value_labels[which(names(value_labels) %in% names(attr(x, "label.table")))] <- attr(x, "label.table")
+    
+  }
+    
+  #haven-style SPSS or Stata variable labels
+   else if (!is.null(unlist(sapply(x, attr, "labels", exact=TRUE)))) {
       
       value_labels <- sapply(x, attr, "labels", exact=TRUE)
       
     }
-    
-  }
-
- 
- 
+  
   
   datatable <- as.data.frame(variable_labels)
 
   #initialize an empty tab-separated textfile to write the metadata to
   cat("\n", file=file, sep="\t")
 
+  ## START HERE - CHECK STATA
   #loop through each variable in mydata and print the variable name, variable labels,
   #and value labels (if not blank) in the text file.
   for (i in 1:ncol(x)){
-    if (!is.null(value_labels[[i]])) {
+    
+    #if the value labels are blank or null, just write a new line with the variable label
+     if (is.null(value_labels[[i]]) || value_labels[[i]] == "") {
+      
+       cat(names(x)[i], "\t", paste(datatable[i,]), "\n\n", file=file, append=TRUE)
+      
+      if (include.summary==TRUE) {
+        capture.output(summary(x[,i]), file=file, append=T) }
+       
+       cat("\n", file=file, append=TRUE)
+     }
+    
+    else {
       #write a line with the variable name tab-separated from the variable label
       cat(names(x)[i], "\t", paste(datatable[i,]), "\n\n", file=file, append=TRUE)
 
@@ -72,27 +96,18 @@ DataDictionary <- function(x, variable_labels = variable_labels, value_labels = 
       #write it in a new indented line in the text file
       for (j in 1:length(a)) {
         cat("\t", a[j], "\n", file=file, append=TRUE)
-      }
+        }
       #add a line to separate the next variable
       cat("\n", file=file, append=TRUE)
       #if summary is TRUE, put in summary of data
 
        if (include.summary==TRUE) {
-        capture.output(summary(x[,i]), file=file, append=T) } else{}
+        capture.output(summary(x[,i]), file=file, append=T) } 
 
       cat("\n", file=file, append=TRUE)
-
-    #if the value labels are blank, just write a new line with the variable label
-   } else {
-      cat(names(x)[i], "\t", paste(datatable[i,]), "\n\n", file=file, append=TRUE)
-
-     if (include.summary==TRUE) {
-       capture.output(summary(x[,i]), file=file, append=T) } else{}
-
-     cat("\n", file=file, append=TRUE)
-  }
-}}
+      }
+    }
 
 
-
+}
 
