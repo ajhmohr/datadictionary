30c094a9cb42a11bb75180e2fbb678067a3bc491
Alicia Hofelich Mohr <hofelich@umn.edu>
2021-08-20 09:33:23 -0500

    updated write.Stata() to take haven style labels

--- R/WriteStataDo.R
+++ R/WriteStataDo.R
@@ -1,6 +1,6 @@
 #' Create .csv and Stata .do file
 #'
-#' This function creates a .csv file and a .do file from a dataframe in R to read into Stata.The data object in R should have variable labels specified in the attribute "variable.labels" and value labels in a list attribute "value labels", with both attributes having the same length as the number of variables in the data frame. 
+#' This function creates a .csv file and a .do file from a dataframe in R to read into Stata.The data object in R should have haven-style labels; variable labels specified in the each variable's attribute "label" and value labels in a list attribute "labels".
 #' As a compatibility check for Stata, any periods (".") in variable names will be replaced with underscores ("_"), all NA values will be replaced with the standard missing value in Stata ("."), and all variables will be cohersed to character to avoid factor conversion issues with Stata. 
 #' @param df A dataframe
 #' @param tempcodefile The file name of Stata .do file that will be written to read in the csv data and apply the variable and value labels. 
@@ -8,8 +8,10 @@
 #' @keywords stata
 #' @keywords do file
 #' @export
-#' @examples iris #Create a Stata do file and csv for iris dataset; attr(iris, "variable.labels") = c("Length of Sepal in centimeters", "Width of Sepal in centimeters", "Length of petal in centimeters", "Width of Petal in centimeters", "Species of iris"); attr(iris, "value.labels") = c(rep("", ncol(iris))); write.Stata(iris, tempcodefile="Irisreadin.do", datafile="Irisdata.csv")
+#' @examples iris #Create a Stata do file and csv for iris dataset; labels= c("Length of Sepal in centimeters", "Width of Sepal in centimeters", "Length of petal in centimeters", "Width of Petal in centimeters", "Species of iris"); for (i in 1:ncol(iris)){  attr(iris[,i], "label") <- labels[i]}; write.Stata(iris, codefile="Irisreadin.do", datafile="Irisdata.csv")
 #' DataDictionary()
+
+
 
 
 write.Stata <- function(df, codefile, datafile) {
@@ -24,18 +26,23 @@ write.Stata <- function(df, codefile, datafile) {
   cat("Writing Data file...", "\n")
   write.table(df1, file = datafile, sep="\t", eol="\r\n", row = FALSE)
   
+  #make sure it is a data frame (and not a tibble) before parsing metadata
+  if (!is.data.frame(df)) {
+    df <- as.data.frame(df)
+  }
+  
   cat("Writing Syntax file...", "\n")
   #set up internal object to collect text and speed up write
-  tempcodefile <- textConnection("codetext", "w", encoding = "UTF-8")
+  tempcodefile <- textConnection("codetext", "w")
   
   cat("import delimited using", adQuote(paste(getwd(), datafile, sep="/")), ", delim(\"\\t\") clear case(\"preserve\")", "\n\n", file=tempcodefile) 
   
   for (i in 1:ncol(df)) {
-    if (attr(df, "variable.labels")[i] != "") {
-      cat("label var", names(df)[i], adQuote(gsub("\"", "", attr(df, "variable.labels")[i])), "\n", file=tempcodefile, append=T)}
+    if (!is.null(attr(df[,i], "label", exact = T))) {
+      cat("label var", names(df)[i], adQuote(gsub("\"", "", attr(df[,i], "label", exact = T))), "\n", file=tempcodefile, append=T)}
     
-    if (attr(df, "value.labels")[i] != "") { 
-      cat("label define", paste(names(df)[i], "label", sep = "_"),  paste(attr(df, "value.labels")[[i]], adQuote(as.character(names(attr(df, "value.labels")[[i]]))), collapse=" "), "\n", file=tempcodefile, append=T)
+    if (!is.null(attr(df[,i], "labels", exact = T))) { 
+      cat("label define", paste(names(df)[i], "label", sep = "_"),  paste(attr(df[,i], "labels", exact = T), adQuote(as.character(names(attr(df[,i], "labels", exact = T)))), collapse=" "), "\n", file=tempcodefile, append=T)
       cat("label values", names(df)[i], paste(names(df)[i], "label", sep = "_"), "\n", file=tempcodefile, append=T)
     }	
   }
